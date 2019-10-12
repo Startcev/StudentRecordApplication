966c8009741fc999f56ce41c0c7e897332b10e44
Сергей <sergey56-11@live.ru>
2019-10-12 22:29:25 +0500

    Созданы два класса описывающие стидента и сохранение файла

---
+++ FileReaderInteractor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace StudentRecordApplication
+{
+    // клас для записи и чтения фалов
+    [Serializable]
+    class FileReaderInteractor
+    {
+        static public void safeObjects(object student, String fileName)
+        {
+            // Создаем поток для записи в файл
+            FileStream myStream = File.Create(fileName);
+
+            // Помещаем объектный граф в поток в двоичном формате
+            BinaryFormatter myBinaryFormat = new BinaryFormatter();
+            myBinaryFormat.Serialize(myStream, student);
+            myStream.Close();
+        }
+
+        // записать список пользователей
+        static public void safeObjects(List<object> listStudent, String fileName)
+        {
+            // в цикле записываем все объекты
+            listStudent.ForEach(delegate (object student)
+            {
+                safeObjects(student, fileName);
+            });
+        }
+
+        // загрузить объект объектов 
+        static public object loadObject(String fileName)
+        {
+            // Создаем поток для записи в файл
+            FileStream myStream = File.OpenRead(fileName);
+
+            // Помещаем объектный граф в поток в двоичном формате
+            BinaryFormatter myBinaryFormat = new BinaryFormatter();
+            object obj = myBinaryFormat.Deserialize(myStream);
+            myStream.Close();
+            return obj;
+        }
+    }
+}
